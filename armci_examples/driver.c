d387da21ef63ce635e103f3b646036fea01eaef4
jeff.science <jeff.science@2a2e761a-6ae5-11de-9593-3d8b33745fea>
2009-08-22 18:57:51 +0000

    This is a very simple test of ARMCI_Get.  It has only been tested on my laptop so far.

---
+++ armci_examples/driver.c
@@ -0,0 +1,98 @@
+/***************************************************************************
+ *   Copyright (C) 2009 by Jeff Hammond                                    *
+ *   jeff.science@gmail.com                                                *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#include "driver.h"
+
+int simple(int me, int nproc, int len);
+
+int main(int argc, char **argv)
+{
+	int me,nproc;
+    int test;
+    int status;
+
+    MPI_Init(&argc, &argv);
+    ARMCI_Init();
+
+#ifdef HPC_PROFILING
+    HPM_Init();
+#endif
+
+    MPI_Comm_rank(MPI_COMM_WORLD,&me);
+    MPI_Comm_size(MPI_COMM_WORLD,&nproc);
+
+    if (argc > 1){
+        test = atoi(argv[1]);
+    } else {
+        test = 1;
+    }
+    if (me == 0){
+        printf("Running test %d\n",test);
+        fflush(stdout);
+    }
+
+#ifdef DEBUG
+    if(me == 0){
+       printf("The result of MPI_Comm_size is %d\n",nproc);
+       fflush(stdout);
+    }
+#endif
+
+    if (test == 1){
+
+        int len;
+        if (argc > 2){
+            len = atoi(argv[2]);
+        } else {
+            len = 1000;
+        }
+
+        if(me == 0){
+            printf("Running simple with %d processes and len = %d\n",nproc,len);
+            fflush(stdout);
+        }
+
+        status = simple(me,nproc,len);
+        if(status != 0){
+            if (me == 0){
+                printf("%s: simple() failed at line %d\n",__FILE__,__LINE__);
+                fflush(stdout);
+            }
+        }
+
+    } else {
+        if(me == 0){
+            printf("Invalid test number (%d) requested\n",test);
+            fflush(stdout);
+        }
+    }
+
+#ifdef HPC_PROFILING
+    HPM_Print();
+#endif
+
+    ARMCI_Finalize();
+    MPI_Finalize();
+
+    return(0);
+}
+
+
+
---
+++ armci_examples/driver.h
@@ -0,0 +1,54 @@
+/***************************************************************************
+ *   Copyright (C) 2008 by Jeff Hammond                                    *
+ *   jeff.science@gmail.com                                                *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#ifndef DRIVER_H
+#define DRIVER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <math.h>
+#include <time.h>
+
+#include "armci.h"
+#include "mpi.h"
+
+/*
+#ifdef USE_GSL
+    #include "essl.h"
+#endif
+
+#ifdef USE_GSL
+    #include "gsl_math.h"
+    #include "gsl_cblas.h"
+#endif
+*/
+
+#ifdef HPM_PROFILING
+    void HPM_Init(void);
+    void HPM_Start(char *);
+    void HPM_Stop(char *);
+    void HPM_Print(void);
+#endif
+
+double start,finish,timing;
+
+#endif
