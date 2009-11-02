c76abfec8df2ff4a677c9cbf7a0113de4db36a06
jeff.science <jeff.science@2a2e761a-6ae5-11de-9593-3d8b33745fea>
2009-11-02 03:41:18 +0000

    440 -> 450 in Makefile

--- dcmf_examples/rma_test.c
+++ dcmf_examples/rma_test.c
@@ -25,6 +25,7 @@
 #include <math.h>
 #include <time.h>
 
+#include "dcmf.h"
 #include "mpi.h"
 
 /***************************************************************/
@@ -98,44 +99,67 @@ unsigned long long getticks(void)
 
 /***************************************************************/
 
+     void function(void* clientdata, DCMF_Error_t* err)
+     {
+         fprintf( stdout , "Put is done\n" );
+         *((int*)clientdata) = 1;
+     }
+
+/***************************************************************/
+
      typedef struct {
-         void* addr;
+         void* buffer;
+         size_t sizetype;
+         DCMF_Memregion_t memregion;
      } RMA_Memregion;
 
      typedef struct {
-         int done;
+         volatile int done;
      } RMA_Request;
+
+    void fake_MPI_RMA_Memregion_create( RMA_Memregion* mem );
+
+    void fake_MPI_RMA_Memregion_destroy( RMA_Memregion* mem );
+
+    void fake_MPI_RMA_Memregion_exchange( RMA_Memregion* local ,
+                                          RMA_Memregion* remote ,
+                                          int rank );
+
+    int datatype_check( MPI_Datatype origin_datatype , 
+                        MPI_Datatype target_datatype ,
+                        MPI_Comm comm , 
+                        int rank );
 
     void fake_MPI_RMA_xfer( int rma_optype , 
                             void* origin_addr , 
                             int origin_count ,
-                            int origin_datatype , 
+                            MPI_Datatype origin_datatype , 
                             RMA_Memregion* target_mem , 
                             int target_disp , 
                             int target_count ,
-                            int target_datatype , 
+                            MPI_Datatype target_datatype , 
                             int target_rank , 
                             MPI_Comm comm , 
                             int RMA_Attributes ,
                             RMA_Request* request );
 
-    void fake_MPI_RMA_put_bytes( void* origin_addr , 
-                                 int count,
-                                 RMA_Memregion* target_addr , 
-                                 int target_disp , 
-                                 int target_rank , 
-                                 MPI_Comm comm , 
-                                 int RMA_Attributes ,
-                                 RMA_Request* request );
+    void fake_MPI_RMA_putb( void* origin_addr , 
+                            int count,
+                            RMA_Memregion* target_addr , 
+                            int target_disp , 
+                            int target_rank , 
+                            MPI_Comm comm , 
+                            int RMA_Attributes ,
+                            RMA_Request* request );
 
-    void fake_MPI_RMA_get_bytes( void* origin_addr , 
-                                 int count,
-                                 RMA_Memregion* target_addr , 
-                                 int target_disp , 
-                                 int target_rank , 
-                                 MPI_Comm comm , 
-                                 int RMA_Attributes ,
-                                 RMA_Request* request );
+    void fake_MPI_RMA_getb( void* origin_addr , 
+                            int count,
+                            RMA_Memregion* target_addr , 
+                            int target_disp , 
+                            int target_rank , 
+                            MPI_Comm comm , 
+                            int RMA_Attributes ,
+                            RMA_Request* request );
 
 /***************************************************************/
 
@@ -146,11 +170,11 @@ int main(int argc, char **argv)
 
     void* origin_addr ;
     int origin_count ;
-    int origin_datatype ;
+    MPI_Datatype origin_datatype ;
     RMA_Memregion* target_mem ;
     int target_disp ;
     int target_count ;
-    int target_datatype ;
+    MPI_Datatype target_datatype ;
     int target_rank ;
     MPI_Comm comm ;
     RMA_Request* request ;
@@ -172,9 +196,20 @@ int main(int argc, char **argv)
     OUTPUT2( "me    = %d\n" , me );
     OUTPUT2( "nproc = %d\n" , nproc );
 
-    unsigned long int len = { ( argc > 1 ) ? ( atol(argv[1]) ) : ( 100 ) };
-    OUTPUT2( "length of message = %ld\n" , len );
-
+    int version = { ( argc > 1 ) ? ( atoi(argv[1]) ) : ( 1 ) };
+    if ( version == 1 )
+    {
+        OUTPUT("testing Vinod's rich RMA interface\n");
+    }
+    else if ( version == 2 )
+    {
+        OUTPUT("testing Jeff's minimalist RMA interface\n");
+    }
+    else
+    {
+        OUTPUT("you must choose 1 or 2!\n");
+    }
+ 
     LINE_STAMP
 
 /***************************************************************/
@@ -194,11 +229,14 @@ int main(int argc, char **argv)
                        request );
     t1 = getticks();
 
+    LINE_STAMP
+
 /***************************************************************/
 
 #ifdef HPC_PROFILING
     HPM_Stop("all");
     HPM_Print();
+    HPM_Print_flops();
 #endif
 
     MPI_Finalize();
@@ -206,43 +244,122 @@ int main(int argc, char **argv)
     return(0);
 }
 
+/***************************************************************/
+
+    void fake_MPI_RMA_Memregion_create( RMA_Memregion* mem )
+    {
+        RMA_Memregion local;
+        local = *mem;        
+        size_t bytes_out;
+        DCMF_Memregion_create(&local.memregion, &bytes_out, local.sizetype, local.buffer, 0);
+        if ( bytes_out != local.sizetype )
+        {
+            fprintf( stdout , "DCMF_Memregion_create failed" );
+            MPI_Abort( MPI_COMM_WORLD , 911 );
+        }
+    }
+
+    void fake_MPI_RMA_Memregion_destroy( RMA_Memregion* mem )
+    {
+        RMA_Memregion local;
+        local = *mem;        
+        DCMF_Memregion_destroy(&local.memregion);
+    }
+
+    void fake_MPI_RMA_Memregion_exchange( RMA_Memregion* local ,
+                                          RMA_Memregion* remote ,
+                                          int rank )
+    {
+        MPI_Status status;
+        MPI_Sendrecv(&local,  sizeof(local), MPI_CHAR, rank, 0,
+                     &remote, sizeof(remote), MPI_CHAR, rank, 0,
+                     MPI_COMM_WORLD, &status);
+    }
+
+    int datatype_check( MPI_Datatype origin_datatype ,
+                        MPI_Datatype target_datatype ,
+                        MPI_Comm comm , 
+                        int remote_rank)
+    {
+        fprintf( stdout , "datatype_check not implemented" );
+        MPI_Abort( comm , 911 );
+        return(0);
+    }
+
+/***************************************************************/
+
     void fake_MPI_RMA_xfer( int rma_optype , 
                             void* origin_addr , 
                             int origin_count ,
-                            int origin_datatype , 
+                            MPI_Datatype origin_datatype , 
                             RMA_Memregion* target_addr , 
                             int target_disp , 
                             int target_count ,
-                            int target_datatype , 
+                            MPI_Datatype target_datatype , 
                             int target_rank , 
                             MPI_Comm comm , 
                             int RMA_Attributes ,
-                            RMA_Request* request ){
+                            RMA_Request* request )
+    {
 
+        int result;
+
+        if ( origin_datatype != target_datatype && target_datatype != MPI_DATATYPE_NULL )
+        {
+            result = datatype_check( origin_datatype , target_datatype , comm , target_rank );
+            if ( result != 0 )
+            {
+                fprintf( stdout , "origin_datatype does not match target_datatype" );
+                MPI_Abort( comm , 911 );
+            }
+        }
+
+        switch(rma_optype)
+        {
+            case RMA_PUT:
+                
+                break;
+
+            case RMA_GET:
+                fprintf( stdout , "RMA_GET not implemented" );
+                MPI_Abort( comm , 911 );
+                break;
+
+            case RMA_ACC_SUM:
+                fprintf( stdout , "RMA_ACC_SUM not implemented" );
+                MPI_Abort( comm , 911 );
+                break;
+
+            case RMA_ACC_PROD:
+                fprintf( stdout , "RMA_ACC_PROD not implemented" );
+                MPI_Abort( comm , 911 );
+                break;
+
+            case RMA_ACC_LXOR:
+                fprintf( stdout , "RMA_ACC_LXOR not implemented" );
+                MPI_Abort( comm , 911 );
+                break;
+
+            default:
+                fprintf( stdout , "Invalid rma_optype" );
+                MPI_Abort( comm , 911 );
+                break;
+        }
+        
 
     }
 
-    void fake_MPI_RMA_put_bytes( void* origin_addr , 
-                                 int count,
-                                 RMA_Memregion* target_addr , 
-                                 int target_disp , 
-                                 int target_rank , 
-                                 MPI_Comm comm , 
-                                 int RMA_Attributes ,
-                                 RMA_Request* request ){
+/***************************************************************/
 
-
-    }
-
-    void fake_MPI_RMA_get_bytes( void* origin_addr , 
-                                 int count,
-                                 RMA_Memregion* target_addr , 
-                                 int target_disp , 
-                                 int target_rank , 
-                                 MPI_Comm comm , 
-                                 int RMA_Attributes ,
-                                 RMA_Request* request ){
+    void fake_MPI_RMA_putb( void* origin_addr , 
+                            int count,
+                            RMA_Memregion* target_addr , 
+                            int target_disp , 
+                            int target_rank , 
+                            MPI_Comm comm , 
+                            int RMA_Attributes ,
+                            RMA_Request* request )
+    {
 
     }
-
 
