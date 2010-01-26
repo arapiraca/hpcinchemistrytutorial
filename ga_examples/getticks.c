1cab75051e7ec0a1f25841294b80bb8588b5c78f
jeff.science <jeff.science@2a2e761a-6ae5-11de-9593-3d8b33745fea>
2010-01-26 05:10:25 +0000

    oops

--- ga_examples/getticks.c
+++ ga_examples/getticks.c
@@ -17,7 +17,7 @@ static __inline__ unsigned long long getticks(void)
 static __inline__ unsigned long long getticks(void)
 {
   unsigned hi, lo;
-  __asm__ __volatile__ ("getticks" : "=a"(lo), "=d"(hi));
+  __asm__ __volatile__ ("rdtsc" : "=a"(lo), "=d"(hi));
   return ( (unsigned long long)lo)|( ((unsigned long long)hi)<<32 );
 }
 
