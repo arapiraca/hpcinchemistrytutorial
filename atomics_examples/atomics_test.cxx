3093e6b50c04a505de3a85ca23c099705692ba9f
jeff.science <jeff.science@2a2e761a-6ae5-11de-9593-3d8b33745fea>
2010-02-13 16:33:22 +0000

    this test code is to show how locks are implemented in TBB and BGP atomics for DCMF.

---
+++ atomics_examples/atomics_test.cxx
@@ -0,0 +1,132 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define TBB_ATOMICS
+
+#if defined(TBB_ATOMICS)
+
+#include <tbb/atomic.h>
+typedef tbb::atomic<uint32_t> atomic_t;
+
+#define atomic_set(m,val)                               \
+({                                                      \
+    m = val;                                            \
+})
+// effect: m++
+#define atomic_inc(m)                                   \
+({                                                      \
+    m.fetch_and_increment();                            \
+})
+// effect:
+//
+#define atomic_lock(m)                                  \
+({                                                      \
+    while (!m.compare_and_swap(1,0));                   \
+})
+// effect:
+// if m=0 then m=1 and return 0
+// if m=1 then         return 1
+#define atomic_trylock(m)                               \
+({                                                      \
+    m.compare_and_swap(1,0);                            \
+})
+// effect: m=0
+#define atomic_unlock(m)                                \
+({                                                      \
+    m = 0;                                              \
+})
+
+#elif defined(BGP_ATOMICS)
+
+#include <bpcore/bgp_atomic_ops.h>
+typedef _BGP_Atomic atomic_t;
+
+#define atomic_set(m, val)                              \
+({                                                      \
+    m.atom = val;                                       \
+})
+// effect: m++
+#define atomic_inc(m)                                   \
+({                                                      \
+    _bgp_fetch_and_add(&m, 1);                          \
+})
+// effect:
+// 
+#define atomic_lock(m)                                  \
+({                                                      \
+  uint32_t zero = 0;                                    \
+  while (!_bgp_compare_and_swap(&m, &zero, 1)) {zero=0;}\
+})
+// effect:
+// if m=0 then m=1 and return 0
+// if m=1 then         return 1
+#define atomic_trylock(m)                               \
+({                                                      \
+  uint32_t zero = 0;                                    \
+  _bgp_compare_and_swap(&m, &zero, 1);                  \
+})
+// effect: m=0
+#define atomic_unlock(m)                                \
+({                                                      \
+    m.atom = 0;                                         \
+})
+
+#endif // TBB_ATOMICS
+
+
+int main(){
+
+    uint32_t i,j;
+    atomic_t _lock,_copy;
+    uint32_t l,c;
+
+    printf("==================================\n");
+    for (i=0;i<10;i++){
+        j=i%2;
+        atomic_set(_lock,j);
+        l=_lock;
+        printf("after atomic_set(_lock,%d), _lock = %d\n",j,l);
+    }
+    
+    printf("==================================\n");
+    for (i=0;i<10;i++){
+        j=i%2;
+        atomic_set(_lock,j);
+        _copy=_lock;
+        c=_copy;
+        atomic_lock(_lock);
+        l=_lock;
+        printf("atomic_lock before: _lock = %d after: _lock = %d\n",c,l);
+    }
+
+    printf("==================================\n");
+    for (i=0;i<10;i++){
+        j=i%2;
+        atomic_set(_lock,j);
+        _copy=_lock;
+        c=_copy;
+        atomic_unlock(_lock);
+        l=_lock;
+        printf("atomic_unlock before: _lock = %d after: _lock = %d\n",c,l);
+    }
+
+    //printf("==================================\n");
+    //int rc = atomic_trylock(_lock);
+
+    printf("==================================\n");
+    return(0);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
