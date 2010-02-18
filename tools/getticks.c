9d61897fc2ae1a02641c3285aaeeef39c2adc97d
jeff.science <jeff.science@2a2e761a-6ae5-11de-9593-3d8b33745fea>
2010-02-18 21:32:58 +0000

    ...

--- tools/getticks.c
+++ tools/getticks.c
@@ -1,66 +1,42 @@
-#ifdef HAVE_DCMF_TIMEBASE
+#ifdef BGP
+    #ifdef HAVE_DCMF_TIMEBASE
+        #include "dcmf.h"
+        __inline__ unsigned long long getticks(void) { return DCMF_Timebase(); }
+    #else
+/* author: azutomo Yoshii kazutomo@mcs.anl.gov from http://www.mcs.anl.gov/~kazutomo/getticks.html */
+        __inline__ unsigned long long getticks(void)
+        {
+          unsigned long long int result=0;
+          unsigned long int upper, lower,tmp;
+          __asm__ volatile(
+                        "0:                  \n"
+                        "\tmftbu   %0           \n"
+                        "\tmftb    %1           \n"
+                        "\tmftbu   %2           \n"
+                        "\tcmpw    %2,%0        \n"
+                        "\tbne     0b         \n"
+                        : "=r"(upper),"=r"(lower),"=r"(tmp)
+                        );
+          result = upper;
+          result = result<<32;
+          result = result|lower;
 
-#include "dcmf.h"
+          return(result);
+        }
+    #endif
+#endif
 
-static __inline__ unsigned long long getticks(void)
-{
-     return DCMF_Timebase();
-}
-
-
-#else
-
-/*
-author: azutomo Yoshii kazutomo@mcs.anl.gov
-from http://www.mcs.anl.gov/~kazutomo/getticks.html
-*/
-
-#if defined(__i386__)
-
-static __inline__ unsigned long long getticks(void)
-{
-  unsigned long long int x;
-     __asm__ volatile (".byte 0x0f, 0x31" : "=A" (x));
-     return x;
-}
-
-#elif defined(__x86_64__)
-
-static __inline__ unsigned long long getticks(void)
+#ifdef LINUX64
+/* author: Kazutomo Yoshii kazutomo@mcs.anl.gov from http://www.mcs.anl.gov/~kazutomo/getticks.html */
+__inline__ unsigned long long getticks(void)
 {
   unsigned hi, lo;
   __asm__ __volatile__ ("rdtsc" : "=a"(lo), "=d"(hi));
   return ( (unsigned long long)lo)|( ((unsigned long long)hi)<<32 );
 }
+#endif
 
-#elif defined(__powerpc__)
-
-static __inline__ unsigned long long getticks(void)
-{
-  unsigned long long int result=0;
-  unsigned long int upper, lower,tmp;
-  __asm__ volatile(
-                "0:                  \n"
-                "\tmftbu   %0           \n"
-                "\tmftb    %1           \n"
-                "\tmftbu   %2           \n"
-                "\tcmpw    %2,%0        \n"
-                "\tbne     0b         \n"
-                : "=r"(upper),"=r"(lower),"=r"(tmp)
-                );
-  result = upper;
-  result = result<<32;
-  result = result|lower;
-
-  return(result);
-}
-
-#else
-
-#include <time.h>
-unsigned long long getticks(void) { return (unsigned long long) clock(); }
-
-#endif // targets
-
-#endif // HAVE_DCMF_TIMEBASE
-
+#ifdef UNKNOWN_STUPID_LINUX_TARGET
+    #include <time.h>
+    unsigned long long getticks(void) { return (unsigned long long) clock(); }
+#endif
